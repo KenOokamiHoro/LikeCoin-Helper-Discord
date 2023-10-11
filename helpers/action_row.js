4567016c49e0b4b9ad46c59e02f45a4a8854ade6
KenOokamiHoro <kenookamihoro@gmail.com>
2023-10-11 19:45:22 +0800

    fix 'proposal_id' use after API Changes

--- helpers/action_row.js
+++ helpers/action_row.js
@@ -5,17 +5,17 @@ function proposal_action_row(proposal) {
     const bigdipper = new ButtonBuilder()
         .setLabel(`${bigdipper_tips[proposal.status]}`)
         .setStyle('Link')
-        .setURL(`https://bigdipper.live/likecoin/proposals/${proposal.proposal_id}`);
+        .setURL(`https://bigdipper.live/likecoin/proposals/${proposal.id}`);
 
     const mintscan = new ButtonBuilder()
         .setLabel(`${mintscan_tips[proposal.status]}`)
         .setStyle('Link')
-        .setURL(`https://www.mintscan.io/likecoin/proposals/${proposal.proposal_id}`);
+        .setURL(`https://www.mintscan.io/likecoin/proposals/${proposal.id}`);
 
     const stake_like_co = new ButtonBuilder()
         .setLabel(`${stake_like_co_tips[proposal.status]}`)
         .setStyle('Link')
-        .setURL(`https://dao.like.co/proposals/${proposal.proposal_id}`);
+        .setURL(`https://dao.like.co/proposals/${proposal.id}`);
 
     return new ActionRowBuilder()
     .addComponents(bigdipper, mintscan, stake_like_co)
